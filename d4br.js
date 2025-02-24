8d61255204d24da6efebe67303a3a41e2e9f0a02
Евгений Афанасьев <jukpochta@gmail.com>
2025-02-24 06:57:01 -0300

    apply fix skills name

--- d4br.js
+++ d4br.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         d4builds rus
 // @namespace    d4br
-// @version      0.15.1
+// @version      0.15.2
 // @description  Перевод для d4builds
 // @author       jukryt
 // @match        https://d4builds.gg/*
@@ -14,21 +14,21 @@
 // @supportURL   https://github.com/jukryt/d4br/issues
 // @grant        GM_getResourceText
 
-// @resource     aspect_en    https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/en/aspect.json
-// @resource     glyph_en     https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/en/glyph.json
-// @resource     unq_item_en  https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/en/unq_item.json
-// @resource     leg_node_en  https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/en/leg_node.json
-// @resource     rune_en      https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/en/rune.json
-// @resource     skill_en     https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/en/skill.json
-// @resource     temper_en    https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/en/temper.json
+// @resource     aspect_en    https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/en/aspect.json
+// @resource     glyph_en     https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/en/glyph.json
+// @resource     unq_item_en  https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/en/unq_item.json
+// @resource     leg_node_en  https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/en/leg_node.json
+// @resource     rune_en      https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/en/rune.json
+// @resource     skill_en     https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/en/skill.json
+// @resource     temper_en    https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/en/temper.json
 
-// @resource     aspect_ru    https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/ru/aspect.json
-// @resource     glyph_ru     https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/ru/glyph.json
-// @resource     unq_item_ru  https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/ru/unq_item.json
-// @resource     leg_node_ru  https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/ru/leg_node.json
-// @resource     rune_ru      https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/ru/rune.json
-// @resource     skill_ru     https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/ru/skill.json
-// @resource     temper_ru    https://raw.githubusercontent.com/jukryt/d4br/3ed3bb4dfe9656c8e4810604986701978d6c3eab/ru/temper.json
+// @resource     aspect_ru    https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/ru/aspect.json
+// @resource     glyph_ru     https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/ru/glyph.json
+// @resource     unq_item_ru  https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/ru/unq_item.json
+// @resource     leg_node_ru  https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/ru/leg_node.json
+// @resource     rune_ru      https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/ru/rune.json
+// @resource     skill_ru     https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/ru/skill.json
+// @resource     temper_ru    https://raw.githubusercontent.com/jukryt/d4br/0cd175726f37c8e0647bb19bc53bc0e33b72b86f/ru/temper.json
 
 // ==/UserScript==
 
