47c5cb8afd41212bc88e21b88396a2077ba2ac4e
Евгений Афанасьев <jukpochta@gmail.com>
2025-03-01 17:01:44 -0300

    split code

---
+++ d4maxroll.js
@@ -0,0 +1,259 @@
+class D4MaxrollProcessor {
+    constructor() {
+        this.sourceLanguage = new EnglishLanguage();
+        this.targetLanguage = new RussianLanguage();
+    }
+
+    mutationObserverCallback(mutations) {
+        for (const mutation of mutations) {
+            if (mutation.type === "childList") {
+                if (mutation.target.id === "uitools-tooltip-root") {
+                    for (const newNode of mutation.addedNodes) {
+                        if (newNode.className === "uitools-tooltip-frame ui-tooltip-active") {
+                            // legendary: aspect, leg node, glyph, rune
+                            if (newNode.querySelector("div.d4t-tip-legendary")) {
+                                const titleNodes = newNode.querySelectorAll("div.d4t-title");
+                                const subTitleNode = newNode.querySelector("div.d4t-sub-title");
+                                for (const titleNode of titleNodes) {
+                                    if (this.legNodeNameProcess(titleNode) ||
+                                        this.glyphNameProcess(titleNode) ||
+                                        this.runeNameProcess(titleNode) ||
+                                        this.aspectNameProcess(titleNode, subTitleNode)) {
+                                        break;
+                                    }
+                                }
+
+                                const temperNodes = newNode.querySelectorAll("li.d4t-list-tempered");
+                                for (const temperNode of temperNodes) {
+                                    this.temperNameProcess(temperNode);
+                                }
+                            }
+                            // rare: glyph, rune
+                            else if (newNode.querySelector("div.d4t-tip-rare")) {
+                                const titleNodes = newNode.querySelectorAll("div.d4t-title");
+                                for (const titleNode of titleNodes) {
+                                    if (this.glyphNameProcess(titleNode) ||
+                                        this.runeNameProcess(titleNode)) {
+                                        break;
+                                    }
+                                }
+                            }
+                            // magic: rune
+                            else if (newNode.querySelector("div.d4t-tip-magic")) {
+                                const titleNodes = newNode.querySelectorAll("div.d4t-title");
+                                for (const titleNode of titleNodes) {
+                                    if (this.runeNameProcess(titleNode)) {
+                                        break;
+                                    }
+                                }
+                            }
+                            // unq item
+                            else if (newNode.querySelector("div.d4t-tip-unique")) {
+                                const titleNode = newNode.querySelector("div.d4t-title");
+                                if (titleNode) {
+                                    this.unqItemNameProcess(titleNode);
+                                }
+                            }
+                            // mythic item
+                            else if (newNode.querySelector("div.d4t-tip-mythic")) {
+                                const titleNode = newNode.querySelector("div.d4t-title");
+                                if (titleNode) {
+                                    this.unqItemNameProcess(titleNode);
+                                }
+                            }
+                            // skill
+                            else if (newNode.querySelector("div.d4t-tip-skill")) {
+                                const skillTitleNode = newNode.querySelector("div.d4t-title");
+                                if (skillTitleNode) {
+                                    this.skillNameProcess(skillTitleNode);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    getCharClassName() {
+        const classNameTitle =
+            document.querySelector("div.d4t-PlannerLink div.d4t-title") ??  // guide
+            document.querySelector("div.header_Header__buildTitle__WS8cB"); // planner
+
+        return classNameTitle?.innerText;
+    }
+
+    aspectNameProcess(titleNode, subTitleNode) {
+        if (!subTitleNode) {
+            return false;
+        }
+
+        const className = "d4br_aspect_name";
+        const subTitleValue = subTitleNode.innerText;
+        // aspect node
+        if (subTitleValue === "Legendary Aspect") {
+            return this.nodeProcess(titleNode, className, Language.aspects, true);
+        }
+        // item node
+        else {
+            const titleValue = titleNode.innerText;
+            const self = this;
+
+            const sourceItem = this.sourceLanguage.aspects.find(i => { return self.aspectNameFilter(i, titleValue); });
+            if (!sourceItem) {
+                return false;
+            }
+
+            const targetItem = this.targetLanguage.aspects.find(i => i.id === sourceItem.id);
+            if (!targetItem) {
+                return false;
+            }
+
+            return this.setTargetValue(titleNode, className, targetItem.name, true);
+        }
+    }
+
+    aspectNameFilter(sourceItem, titleValue) {
+        const aspectIndex = sourceItem.name.indexOf("Aspect");
+        // [Aspect of ...] => [Item_Name of Aspect_Name]
+        if (aspectIndex === 0) {
+            const aspectName = sourceItem.name.substring(6);
+            if (titleValue.endsWith(aspectName)) {
+                return true;
+            }
+        }
+        // [... Aspect] => [Aspect_Name Item_Name]
+        else {
+            const aspectName = sourceItem.name.substring(0, aspectIndex);
+            if (titleValue.startsWith(aspectName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    temperNameProcess(node) {
+        const sourceValue = node.innerText;
+        if (!sourceValue) {
+            return false;
+        }
+
+        const charClassName = this.getCharClassName();
+        if (!charClassName) {
+            return false;
+        }
+
+        const temperValue = sourceValue
+            .replace(/\([^\)]+\)/, "") // (text)
+            .replace(/\[[0-9\. \-]+\]%?/, "") // [values]
+            .trim();
+
+        const tempers = this.sourceLanguage.tempers.filter(i => i.values && (i.class === charClassName || i.class === "All"));
+        let sourceItems = tempers.filter(i => i.values.some(s => {
+            const match = temperValue.match(s)
+            return match &&
+                match.index === 0 &&
+                match[0] === temperValue;
+        }));
+
+        if (sourceItems.length === 0) {
+            return false;
+        }
+
+        if (sourceItems.length > 1) {
+            if (Array.from(new Set(sourceItems.map(i => i.type))).length === 1) {
+                const classItem = sourceItems.find(i => i.class === charClassName);
+                if (classItem) {
+                    sourceItems = [classItem];
+                } else {
+                    sourceItems = [sourceItems[0]];
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        const sourceItem = sourceItems[0];
+        const targetItem = this.targetLanguage.tempers.find(i => i.id === sourceItem.id);
+        if (!targetItem) {
+            return false;
+        }
+
+        let targetTemperName = targetItem.name;
+
+        const sourceTemperType = this.sourceLanguage.temperTypes.find(i => i.name === sourceItem.type);
+        if (sourceTemperType) {
+            const targetTemperType = this.targetLanguage.temperTypes.find(i => i.id === sourceTemperType.id);
+            if (targetTemperType) {
+                targetTemperName = targetTemperType.name + " - " + targetTemperName;
+            }
+        }
+
+        const newNode = document.createElement("div");
+        newNode.style["margin-top"] = "5px";
+        newNode.style.opacity = "0.6";
+        node.parentNode.insertBefore(newNode, node);
+
+        return this.setTargetValue(newNode, "d4br_temper_name", targetTemperName, false);
+    }
+
+    unqItemNameProcess(node) {
+        return this.nodeProcess(node, "d4br_unq_item_name", Language.unqItems, true);
+    }
+
+    skillNameProcess(node) {
+        return this.nodeProcess(node, "d4br_skill_name", Language.skills, true);
+    }
+
+    legNodeNameProcess(node) {
+        return this.nodeProcess(node, "d4br_leg_node_name", Language.legNodes, true);
+    }
+
+    glyphNameProcess(node) {
+        return this.nodeProcess(node, "d4br_glyph_name", Language.glyphs, true);
+    }
+
+    runeNameProcess(node) {
+        return this.nodeProcess(node, "d4br_rune_name", Language.runes, true);
+    }
+
+    nodeProcess(node, className, resourceName, addSourceValue) {
+        const sourceValue = node.innerText;
+        if (!sourceValue) {
+            return false;
+        }
+
+        const sourceItems = this.sourceLanguage.getResource(resourceName).filter(i => i.name === sourceValue);
+        if (sourceItems.length != 1) {
+            return false;
+        }
+
+        const sourceItem = sourceItems[0];
+        const targetItem = this.targetLanguage.getResource(resourceName).find(i => i.id === sourceItem.id);
+        if (!targetItem) {
+            return false;
+        }
+
+        return this.setTargetValue(node, className, targetItem.name, addSourceValue);
+    }
+
+    setTargetValue(node, className, targetValue, addSourceValue) {
+        if (!targetValue) {
+            return false;
+        }
+
+        let htmlValue = this.buildHtmlValue(className, targetValue);
+        if (addSourceValue) {
+            htmlValue += node.innerHTML;
+        }
+
+        node.innerHTML = htmlValue;
+        return true;
+    }
+
+    buildHtmlValue(className, value) {
+        return `<div class="d4br_show ${className}" style="color:darkgray; font-size:18px;">${value}</div>`;
+    }
+}
---
+++ d4mobalytics.js
@@ -0,0 +1,206 @@
+class D4MobalyticsProcessor {
+    constructor() {
+        this.sourceLanguage = new EnglishLanguage();
+        this.targetLanguage = new RussianLanguage();
+    }
+
+    mutationObserverCallback(mutations) {
+        for (const mutation of mutations) {
+            if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+                if (mutation.target.id.startsWith("tippy-")) {
+                    const tippyNode = mutation.target;
+                    // aspect and temper
+                    if (tippyNode.querySelector("div.m-1tii5t")) {
+                        const aspectNameNode = tippyNode.querySelector("p.m-foqf9j");
+                        if (aspectNameNode) {
+                            this.aspectNameProcess(aspectNameNode);
+                        }
+                        const temperNameNodes = tippyNode.querySelectorAll("span.m-1yjh4k8");
+                        for (const temperNameNode of temperNameNodes) {
+                            this.temperNameProcess(temperNameNode);
+                        }
+                    }
+                    // unq item
+                    else if (tippyNode.querySelector("div.m-mqkczm")) {
+                        const unqItemNameNode = tippyNode.querySelector("h4.m-yb0jxq");
+                        if (unqItemNameNode) {
+                            this.unqItemNameProcess(unqItemNameNode);
+                        }
+                    }
+                    // skill
+                    else if (tippyNode.querySelector("div.m-1saunj6")) {
+                        const skillNameNode = tippyNode.querySelector("p.m-foqf9j");
+                        if (skillNameNode) {
+                            this.skillNameProcess(skillNameNode);
+                        }
+                    }
+                    // glyph
+                    else if (tippyNode.querySelector("div.m-yak0pv")) {
+                        const glyphNameNode = tippyNode.querySelector("p.m-pv4zw0");
+                        if (glyphNameNode) {
+                            this.glyphNameProcess(glyphNameNode);
+                        }
+                    }
+                    // leg node
+                    else if (tippyNode.querySelector("div.m-1fwtoiz")) {
+                        const legNameNode = tippyNode.querySelector("p.m-1vrrnd3");
+                        if (legNameNode) {
+                            this.legNodeNameProcess(legNameNode);
+                        }
+                    }
+                    // rune
+                    else if (tippyNode.querySelector("div.m-1m5senx")) {
+                        const runeNameNode = tippyNode.querySelector("p.m-54521m");
+                        if (runeNameNode) {
+                            this.runeNameProcess(runeNameNode);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    getCharClassName() {
+        const classNameTitle =
+            document.querySelector("span.m-a53mf3") ??                           // build
+            document.querySelector("#downshift-0-toggle-button span.m-1sjbyfv"); // planner
+
+        return classNameTitle?.innerText
+            ?.replace("Diablo 4 ", "")
+            ?.replace(" Build", "");
+    }
+
+    aspectNameProcess(node) {
+        return this.nodeProcess(node, "d4br_aspect_name", Language.aspects, true);
+    }
+
+    temperNameProcess(node) {
+        const sourceValue = node.innerText;
+        if (!sourceValue) {
+            return false;
+        }
+
+        const charClassName = this.getCharClassName();
+
+        const sourceItems = this.sourceLanguage.tempers.filter(i => (i.class === charClassName || i.class === "All") && i.name === sourceValue);
+
+        if (sourceItems.length === 0) {
+            return false;
+        }
+
+        if (sourceItems.length > 1) {
+            if (Array.from(new Set(sourceItems.map(i => i.type))).length === 1) {
+                const classItem = sourceItems.find(i => i.class === charClassName);
+                if (classItem) {
+                    sourceItems = [classItem];
+                } else {
+                    sourceItems = [sourceItems[0]];
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        const sourceItem = sourceItems[0];
+        const targetItem = this.targetLanguage.tempers.find(i => i.id === sourceItem.id);
+        if (!targetItem) {
+            return false;
+        }
+
+        let targetTemperName = targetItem.name;
+
+        const sourceTemperType = this.sourceLanguage.temperTypes.find(i => i.name === sourceItem.type);
+        if (sourceTemperType) {
+            const targetTemperType = this.targetLanguage.temperTypes.find(i => i.id === sourceTemperType.id);
+            if (targetTemperType) {
+                targetTemperName = targetTemperType.name + " - " + targetTemperName;
+            }
+        }
+
+        const newNode = document.createElement("div");
+        newNode.style.opacity = "0.6";
+        node.parentNode.insertBefore(newNode, node);
+
+        return this.setTargetValue(newNode, "d4br_temper_name", targetTemperName, false);
+    }
+
+    unqItemNameProcess(node) {
+        return this.nodeProcess(node, "d4br_unq_item_name", Language.unqItems, true);
+    }
+
+    skillNameProcess(node) {
+        return this.nodeProcess(node, "d4br_skill_name", Language.skills, true);
+    }
+
+    glyphNameProcess(node) {
+        const sourceValue = node.innerText;
+        if (!sourceValue) {
+            return false;
+        }
+
+        const glyphMatch = sourceValue.match(/([a-zA-Z ]+) \(Lvl \d+\)/);
+        if (!glyphMatch) {
+            return false;
+        }
+
+        const glyphName = glyphMatch[1];
+        const sourceItem = this.sourceLanguage.glyphs.find(i => i.name === glyphName);
+        if (!sourceItem) {
+            return false;
+        }
+
+        const targetItem = this.targetLanguage.glyphs.find(i => i.id === sourceItem.id);
+        if (!targetItem) {
+            return false;
+        }
+
+        return this.setTargetValue(node, "d4br_glyph_name", targetItem.name, true);
+    }
+
+    legNodeNameProcess(node) {
+        return this.nodeProcess(node, "d4br_leg_node_name", Language.legNodes, true);
+    }
+
+    runeNameProcess(node) {
+        return this.nodeProcess(node, "d4br_rune_name", Language.runes, true);
+    }
+
+    nodeProcess(node, className, resourceName, addSourceValue) {
+        const sourceValue = node.innerText;
+        if (!sourceValue) {
+            return false;
+        }
+
+        const sourceItems = this.sourceLanguage.getResource(resourceName).filter(i => i.name === sourceValue);
+        if (sourceItems.length != 1) {
+            return false;
+        }
+
+        const sourceItem = sourceItems[0];
+        const targetItem = this.targetLanguage.getResource(resourceName).find(i => i.id === sourceItem.id);
+        if (!targetItem) {
+            return false;
+        }
+
+        return this.setTargetValue(node, className, targetItem.name, addSourceValue);
+    }
+
+    setTargetValue(node, className, targetValue, addSourceValue) {
+        if (!targetValue) {
+            return false;
+        }
+
+        let htmlValue = this.buildHtmlValue(className, targetValue);
+        if (addSourceValue) {
+            htmlValue += node.innerHTML;
+        }
+
+        node.innerHTML = htmlValue;
+        return true;
+    }
+
+    buildHtmlValue(className, value) {
+        return `<div class="d4br_show ${className}" style="color:darkgray; font-size:15px;">${value}</div>`;
+    }
+}
