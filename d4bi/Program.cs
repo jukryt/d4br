1acb02d065bfd16716c15b69fe033c1e9467e835
Evgenii Afanasev <jukpochta@gmail.com>
2026-03-12 13:51:25 +0300

    add resource filters

--- d4bi/Program.cs
+++ d4bi/Program.cs
@@ -10,7 +10,6 @@ namespace Importer
         private static async Task Main()
         {
             var appConfig = await AppConfig.LoadAsync();
-
             await ExecuteAsync(appConfig);
         }
 
@@ -18,13 +17,26 @@ namespace Importer
         {
             using var browser = await PuppeteerBrowser.RunAsync(appConfig.MaxBrowserPageCount, appConfig.BrowserRequestTimeout);
             using var reportManager = new ReportManager();
+            await ExecuteAsync(appConfig, browser, reportManager);
+        }
+
+        private static async Task ExecuteAsync(AppConfig appConfig, PuppeteerBrowser browser, ReportManager reportManager)
+        {
+            var filter = Array.Empty<string>();
+            //var filter = new[] {
+            //    "aspect en", "glyph en", "leg_node en", "rune en", "unq_item en", "elixir en",
+            //    "aspect ru", "glyph ru", "leg_node ru", "rune ru", "unq_item ru", "elixir ru" };
+            //var filter = new[] { "skill en" };
+            //var filter = new[] { "temper en" };
+            //var filter = new[] { "skill ru" };
+            //var filter = new[] { "temper ru" };
 
             var runner = new ProcessorRunner(browser, appConfig.MaxProcessorCount);
 
             var tasks = Enumerable.Empty<Task>();
             foreach (var resourceCollection in Resources.GetResources())
             {
-                var resourceTasks = resourceCollection.Infos
+                var resourceTasks = resourceCollection.Infos.Where(x => !filter.Any() || filter.Contains(x.Name))
                     .Select(i => ExecuteAsync(i, resourceCollection.Folder, runner, appConfig, reportManager));
 
                 tasks = tasks.Union(resourceTasks);
