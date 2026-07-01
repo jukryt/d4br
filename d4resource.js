9f82757053260afe12f976f485444f05138e717c
Евгений Афанасьев <jukpochta@gmail.com>
2026-07-01 23:21:41 +0300

    add display property to clone element

--- d4resource.js
+++ d4resource.js
@@ -42,6 +42,7 @@ class ElementBuilder {
         const nodeStyle = window.getComputedStyle(sourceNode);
 
         const targetNode = document.createElement(sourceNode.localName);
+        targetNode.style.display = nodeStyle.getPropertyValue("display");
         targetNode.style.fontFamily = nodeStyle.getPropertyValue("font-family");
         targetNode.style.fontSize = nodeStyle.getPropertyValue("font-size");
         targetNode.style.textAlign = nodeStyle.getPropertyValue("text-align");
